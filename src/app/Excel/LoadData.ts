aec68061870318d968f6a1134f9f6c05a5295fc2
hushunding <hushunding@aliyun.com>
2018-10-07 21:58:04 +0800

    加载完Excel，转换成对象，待添加测试用例

--- src/app/Excel/LoadData.ts
+++ src/app/Excel/LoadData.ts
@@ -1,19 +1,45 @@
 
-import { read, utils, WorkBook, readFile } from 'xlsx'
+import { utils, WorkBook, readFile } from 'xlsx';
 import { existsSync } from 'fs';
+import { assert } from 'chai';
 
 export class ExcelData {
-    wb: WorkBook
+    wb: WorkBook;
     constructor(fileName: string) {
-        if (existsSync(fileName)) {
-            this.wb = readFile(fileName)
-        } else {
-            throw new Error(`${fileName} 不存在`);
-        }
+        assert.ok(existsSync(fileName), `${fileName} 不存在`);
+        this.wb = readFile(fileName);
+
     }
-    checkExcelVersion() {
-        if (this.wb.SheetNames.indexOf('模板版本修订记录') < 0) {
-            throw new Error('文件格式错误:缺少模板版本修订记录页');
+    CheckExcelVersion(expectVersion: string) {
+        assert.oneOf<string>('模板版本修订记录', this.wb.SheetNames, '文件格式错误:缺少模板版本修订记录页');
+        const versionSheet = this.wb.Sheets['模板版本修订记录'];
+        const versiondatas = utils.sheet_to_json(versionSheet, { header: 1 });
+        const version = (versiondatas.pop()[0] as string).trim();
+        assert.strictEqual(version, expectVersion, '版本错误');
+        return '版本检查通过';
+    }
+    CheckExcelFormat(prefix: '输入' | '输出', paramTypeList: string[]) {
+        const MissingParamSheet =
+            paramTypeList.filter((paramType) =>
+                this.wb.SheetNames.findIndex((shName) =>
+                    RegExp(`${prefix}-${paramType}`).test(shName)) < 0);
+        assert.isEmpty(MissingParamSheet, `文件格式错误:缺少参数页-"${MissingParamSheet}"`);
+    }
+    /**
+     * @description 将Excel表格转为Json对象Map，Sheet的首行作为Json的key
+     * @param prefix 数据类别
+     * @param paramTypeList 数据子页
+     * @returns  Json对象Map
+     */
+    GetJson(prefix: '输入' | '输出', paramTypeList: string[]) {
+        const selSheet =
+            this.wb.SheetNames.filter((shName) =>
+                paramTypeList.some((paramType) =>
+                    RegExp(`${prefix}-${paramType}`).test(shName)));
+        const selObj = new Map<string, any>();
+        for (const sh of selSheet) {
+            selObj.set(sh, utils.sheet_to_json(this.wb.Sheets[sh]));
         }
+        return selObj;
     }
 }
--- src/test/ExcelTest.ts
+++ src/test/ExcelTest.ts
@@ -2,17 +2,42 @@ import { assert, expect } from 'chai';
 import 'mocha';
 import { ExcelData } from '../app/Excel/LoadData';
 import { strictEqual } from 'assert';
+import { InputParamTypeList, OutputParamTypeList } from '../app/ParamCheck/ParamCheck';
 
-
+const testExcel = 'data/test/全项测试集.xlsx';
 describe('Excel文件加载测试', () => {
 
-    it("文件不存在测试", () => {
-        expect(() => { new ExcelData("A") }).to.throw(Error, "A 不存在");
+    it('文件不存在测试', () => {
+        expect(() => { const ed: ExcelData = new ExcelData('A'); }).throw(Error, 'A 不存在');
     });
-    it("模板版本页不存在", () => {
-        const ed:ExcelData = new ExcelData("data/test/模板版本页不存在.xlsx")
-        expect(() => { ed.checkExcelVersion() }).to.throw(Error, "文件格式错误:缺少模板版本修订记录页");
+    it('模板版本页不存在', () => {
+        const ed: ExcelData = new ExcelData('data/test/模板版本页不存在.xlsx');
+        expect(() => { ed.CheckExcelVersion(''); }).throw(Error, '文件格式错误:缺少模板版本修订记录页');
     });
-
-
+    describe('模板版本和格式检查', () => {
+        const ed: ExcelData = new ExcelData(testExcel);
+        it('模板版本错误', () => {
+            expect(() => { ed.CheckExcelVersion(''); }).throw(Error, /版本错误/);
+        });
+        it('模板版本正确', () => {
+            expect(() => { ed.CheckExcelVersion('V2.4'); }).not.throw();
+        });
+        it('模板格式错误', () => {
+            expect(() => { ed.CheckExcelFormat('输入', ['其他1', '其他2']); }).throw(/文件格式错误:缺少参数页-/);
+        });
+        it('模板格式正确', () => {
+            expect(() => { ed.CheckExcelFormat('输入', InputParamTypeList); }).not.throw();
+        });
+    });
+    describe('Excel数据加载', () => {
+        const ed: ExcelData = new ExcelData(testExcel);
+        it('输入参数读取', () => {
+            const inputParam =  ed.GetJson('输入', InputParamTypeList);
+            expect(inputParam.size).equals(4) ;
+        });
+        it('输出参数读取', () => {
+            const outputParam =  ed.GetJson('输出', OutputParamTypeList);
+            expect(outputParam.size).equals(3);
+        });
+    });
 });
